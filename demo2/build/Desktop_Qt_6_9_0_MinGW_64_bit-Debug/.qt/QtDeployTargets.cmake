bb76fe0451b7953c51ff01d4e361176dc90c86a6
Aastha Sharma <51191976+aaztha@users.noreply.github.com>
2025-06-09 00:45:44 +0530

    Delete demo2 directory

--- demo2/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_demo2_FILE C:/Qt/Project/demo2/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/demo2.exe)
-set(__QT_DEPLOY_TARGET_demo2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_demo2_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
